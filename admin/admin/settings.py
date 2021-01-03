ef6a68b6e00c025002322743ad646af7fd6424a6
nitheeshmavila <mail2nitheeshmavila@gmail.com>
2021-01-03 22:05:11 +0530

    added admin products app

--- admin/admin/settings.py
+++ admin/admin/settings.py
@@ -80,6 +80,7 @@ WSGI_APPLICATION = 'admin.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': 'db',
